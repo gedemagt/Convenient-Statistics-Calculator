fba41d10e963275fa81c808ead5a4cbea63611c8
gedemagt <gedemagt@gmail.com>
2014-07-22 14:58:51 +0200

    Added default handling as a inner class.

--- src/com/jesper/InputHandlers/InputHandlerManager.java
+++ src/com/jesper/InputHandlers/InputHandlerManager.java
@@ -11,10 +11,13 @@ public class InputHandlerManager {
     private Map<String, InputHandler> inputs = new TreeMap<String, InputHandler>();
     private InputHandler defaultHandler = null;
 
+    public InputHandlerManager() {
+        addInputHandler(new DefaultHandler());
+    }
+
     public void addInputHandler(InputHandler handler) {
         inputs.put(handler.getTag(), handler);
     }
-
 
     public Map<String, InputHandler> getHandlerMap() {return inputs;}
 
@@ -27,29 +30,36 @@ public class InputHandlerManager {
         s = MathParser.parse(s);
 
         int first_space = s.indexOf(" ");
+        
         if(first_space == -1) {
             if(inputs.containsKey(s)) return inputs.get(s).process("");
             if(defaultHandler == null) throw new Exception("No default handler specified");
             else return defaultHandler.process(s);
         }
+
         String tag = s.substring(0, first_space);
         String exp = s.substring(first_space+1);
-        if(tag.equals("default")) {
-            InputHandler h = inputs.get(exp);
-            if (h == null) throw new Exception("Unknown tag: " + exp);
-            else defaultHandler = h;
-            return "New default input: " + exp;
-        }
+
         if(inputs.containsKey(tag)) {
             return inputs.get(tag).process(exp);
         }
         throw new Exception("Unknown tag: " + tag);
     }
 
+    private class DefaultHandler implements InputHandler {
 
-    //private int find(String s, String delimiter, int start) {
-    //    return s.indexOf(delimiter, start);
-    //}
+        public String getHelp(){
+            return "Default help!";
+        }
+        public String getTag(){
+            return "default";
+        }
+        public String process(String s) throws Exception {
+            InputHandler h = inputs.get(s);
+            if (h == null) throw new Exception("Unknown tag: " + s);
+            else defaultHandler = h;
+            return "New default input: " + s;
+        }
 
-
+    }
 }
